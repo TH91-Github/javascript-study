4751697d639e05d12b5ae272c4b975044ca3a95d
TH91-Github <je64768035@gmail.com>
2022-06-17 15:06:01 +0900

    220617 새로 재구성 : swiper까지 완성

--- 220601_test_th/src/main.js
+++ 220601_test_th/src/main.js
@@ -2,242 +2,289 @@ import './scss/main.scss'
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
-const sectionUrl1 = "http://localhost:4000/posts1";
-const sectionUrl2 = "http://localhost:4000/posts2";
-const sectionUrl3 = "http://localhost:4000/options";
 
-let iniData = [];
+/*
+  1. json url 받기
+  2. 구조 생성
+    - HTML commponent 생성
+  3. 데이터를 통한 리스트 생성
+    - slide 구조 생성
+  4. 컴포넌트 구조 APP 삽입
+  5. 생성된 리스트 구조에 new swiper   
+  6. 슬라이드 개별로 이벤트
+    - 클릭한 slide 가운데로 동작 후 모달 실행
+    - 슬라이드 동작 완료 후 실행. 
+  7. 모달 - 슬라이드 개별 이벤트 발동 시 
+    -1 모달 html 구조 생성
+    -2 모달 내 슬라이드 생성
+    -3 딤드 생성 및 이벤트 추가
+      - 모달 및 딤드 삭제 (4-1 동일 기능)
+    -4 모달 닫기 버튼 이벤트 추가
+      -1 닫기 클릭 시 모달 및 딤드 삭제
+*/
 
-const componentFunc = () => {
-  const section = document.querySelectorAll('.section');
-         
-  
-  const sectionList = () => { // 초기 데이터 및 컨텐츠 생성.
-    
+// app 
+const $app = document.getElementById('app');
+const commponentName = "option"; // commponent name
+// base url 
+const serverDataArr = [
+  "http://localhost:4000/posts1",
+  "http://localhost:4000/posts2",
+  "http://localhost:4000/options"
+];
 
-    section.forEach((sectionE,sectionI) =>{ // section 별
-      let _this = sectionE;
-      
-      if(sectionI == 0 ){ // 첫번째일 경우에만 
-        jsonData(sectionUrl1).then((data) => {
-          listPush(_this, dataMapTag(data, sectionI));
-          setTimeout (() =>{ 
-            iniData.push(data); // 데이터 따로 저장
-          },500)
-        })
-        .catch((error) => console.log(error));
-      }else if(sectionI == 1 ){ // 두번째일 경우에만 
-        jsonData(sectionUrl2).then((data) => {
-          listPush(_this, dataMapTag(data, sectionI));
-          setTimeout (() =>{ 
-            iniData.push(data); // 데이터 따로 저장
-          },500)
-        })
-        .catch((error) => console.log(error));
-      }else{
-
-      }
-    })
-    
-  } // e: sectionList
-
-  const init = (() => { 
-    sectionList();
-  })();
-
-} // e: componentFunc
-
-const jsonData = async (jsonUrl) => {
-  const res = await fetch(jsonUrl);
-  const posts = await res.json();
-  return posts
-} // e: jsonData
-
-
-const dataMapTag = (getData, urlNum) => { 
-  let tagData = getData.map((e)=>{
-    let createListData = createList(e,urlNum);
-    return createListData;
-  });
-  return tagData;
+// 1 json - fetch - then - load
+const loadData = (iniUrl) => { 
+  return fetch(iniUrl) 
+  .then((response) => response.json())
+}
+// 1-1 data를 통한 component
+const getDataLoad = (getUrl, getNum) => {
+  loadData(getUrl)
+  .then((data) => {
+    commponentPush();  // commponent 생성
+    commponentContent(data, getNum);  // load Data 활용 및 cont 생성
+  })
+  .catch((error) => console.log(error));
 }
 
-// slider 태그 구조 생성 및 데이터 입력
-const createList = (e, urlNum) => { 
+
+// 2-1  component 기본 구조 생성
+const componentCreate = (cmClass) => {
+  let componentWrap = commonCreateTag("div", "cm-"+cmClass);
+  componentWrap.innerHTML = `
+    <div class="cm-${cmClass}__wrap">
+    </div>
+`;
+  return componentWrap;
+}
+// 2-2 #app에 commponent push
+const commponentPush = () => {
+  $app.appendChild(componentCreate(commponentName));
+}
+
+// 3 받은 데이터를 완성된 list를 cont로 생성 후 전달
+const swiperList = (contName, listData) => {
+  let listWrap = `
+    <div class="cm-${contName}__list swiper">
+      <div class="cm-${contName}__list-wrap swiper-wrapper">
+        ${listForEach(contName, listData)}
+      </div>
+    </div>
+  `;
+  return listWrap;
+}
+
+// 3-1 전달 받은 데이터를 list 가공 후 중첩 시켜 하나의 데이터로 만들고 반환 ↑
+const listForEach = (listmName, listData) => { 
+  let listArr = listExtraction(listmName, listData).reduce((resultEl, currentEl) => resultEl+currentEl)
+  return listArr;
+}
+
+// 3-2 중첩시킬 데이터를 순서대로 list 구조에 뿌린 후 재배열 후 반환 ↑
+const listExtraction = (nameExtraction, dataExtraction) => { 
+  let listPush = dataExtraction.map((mapData)=>{
+    return createList(nameExtraction, mapData);
+  });
+  return listPush;
+}
+
+// 3-3 받은 데이터를 swiper slide 구조에 맞게 생성 후 반환 ↑
+const createList = (itemClass, itemData, urlNum) => { 
   let optTemplate = '';
-  optTemplate += `<a href="#" class="option-info__item" data-id="${e.postId}" data-url-num="${urlNum}">
-      <div class="option-info__item-img">
-        <div class="img"><img src="${e.img}" alt="" /></div>
-      </div>
-      <div class="option-info__item-desc">
-        <p class="tit">${e.tit}</p>
-        <p class="desc">${e.description}</p>
-      </div>
-    </a>`;
+  optTemplate = `
+      <a href="#" class="cm-${itemClass}__list-item swiper-slide" data-id="${itemData.postId}" data-url-num="${urlNum}">
+        <div class="option-info__item-img">
+          <div class="img"><img src="${itemData.img}" alt="" /></div>
+        </div>
+        <div class="option-info__item-desc">
+          <p class="tit">${itemData.tit}</p>
+          <p class="desc">${itemData.description}</p>
+        </div>
+      </a>`;
   return optTemplate;
 }
 
-// list cont 입력
-const listPush = (thisEl, thisCont) => {  
-  let listWrap = thisEl.querySelector('.option-info__list');
-  let listInner = createTag("div","option-info__list-inner");
+// 4. list 구조 commponent wrap에 푸쉬
+const commponentContent = (passData, passNum) => {
+  let thisComponent = $app.querySelectorAll(`.cm-${commponentName}`)[passNum];
+  let thisCommponentWrap = thisComponent.querySelector(`.cm-${commponentName}__wrap`); // commponent__wrap 선택
+  thisCommponentWrap.innerHTML = swiperList(commponentName, passData);
 
-  thisCont.forEach((thisInfo,idx) => {
-    let lists = createTag("div","option-info__list-lists");  // , 
-    lists.innerHTML = thisInfo; // list 담기
-    listInner.appendChild(lists); 
-  })
-  listWrap.appendChild(listInner);
+  let changeOpt = {
+    loop : true,
+    breakpoints : {
+      768 : {
+        slidesPerView : 3,
+        spaceBetween : 30,
+      }
+    },
+    slideToClickedSlide: true, // 클릭한 슬라이드로 이동
+    centeredSlides: true, // 센터
+    on: {
+      init: function (e) {
+        console.log("초기")
+        console.log(e)
+        let $iniEl = e.el; 
+        let slides = e.slides;
+        slides.forEach((el,index) =>{
+          el.addEventListener("click", () => {
+            $iniEl.classList.add("clickSwiper")
+          })
+        });
+      },
+      // slideChange : function () {
+      //   console.log("실행")
+      // },
+      // transitionEnd: function () {
+      //   console.log("테스트")
+      // },
+      // slideChangeTransitionStart: function (e) {
+      //   console.log("시작")
+      //   console.log(e)
+      // },
+      slideChangeTransitionEnd: function (e) {
+        console.log("끝")
+        console.log(e)
+        let $endChkEl = e.el;
 
-
-  if(listWrap.dataset.swiper == "on"){ // 슬라이드 사용 on/off 
-    swiperFunc(listWrap);
-    let slideItem = listInner.querySelectorAll('.option-info__item');
-    slideItem.forEach((thisClick, idx)=>{ // 
-      thisClick.addEventListener("click", () =>{   
-        let urlInfo = thisClick.dataset.urlNum;
-        modalOpen(urlInfo, idx);
-      });
-    })
+        // console.log($endChkEl.getAttribute("class"));
+        console.log($endChkEl.getAttribute("class").indexOf("clickSwiper"))
+        // if($endChkEl.getAttribute("class").indexOf)
+        //if($endChkEl.)
+      },
+    }
   }
+  swiperFunc(thisComponent, changeOpt)// 기준이 되는 commponent , 슬라이드 옵션
 }
 
-/* s: modalLayer */
-const modalOpen = (urlInfo, slideNum) => {
-  let app = document.getElementById('app');
-  app.appendChild(modalCreate())  // 모달 구조 생성
-  modalSwiper(urlInfo, slideNum); // 모달 swiper 생성
-  dimmedFunc("open"); // dimmed 생성
-  return false;
-}
-
-const modalCreate = () => { // 모달창 뼈대
-  let modalLayer = createTag("div","modal-layer");
-  let modalWrap = createTag("div","modal-layer__wrap");
-  let modalInner = createTag("div","modal-layer__inner",[["data-navigation",["on"]],["data-pagination","on"]]);
-  let modalList = createTag("div","modal-layer__list");
-  let closedBox = createTag("div","modal-layer__closed");
-  let closedBtn = createTag("button","btn",[["type","button"]], "닫기");
-
-  closedBox.appendChild(closedBtn);
-  modalInner.appendChild(modalList);
-  modalWrap.appendChild(modalInner);
-  modalWrap.appendChild(closedBox);
-  modalLayer.appendChild(modalWrap);
-  return modalLayer;
-}
-
-const modalSwiper = (urlInfo, slideNum) => { // 모달창 내 슬라이드
-  let modalLayer = document.querySelector('.modal-layer');
-  let loadUrl ="";
-
-  if(urlInfo == 0) loadUrl = sectionUrl1
-  else if(urlInfo == 1) loadUrl = sectionUrl2
-  else console.log("null")
-  
-  jsonData(loadUrl).then((data) => {
-    modalListPuh(modalLayer, modalMapTag(data));
-  })
-
-  const modalListPuh = (modalLayer, modalSlideData) => {
-    let modalSwiperInner = modalLayer.querySelector('.modal-layer__inner');
-    let modalSwiperList = modalLayer.querySelector('.modal-layer__list')
-    modalSlideData.forEach((slideInfo,idx) => {
-      let itmes = createTag("div","option-info__list-lists");  // , 
-      itmes.innerHTML = slideInfo; // list 담기
-      modalSwiperList.appendChild(itmes); 
-    })
-    swiperFunc(modalSwiperInner, slideNum); // slideNum 해당 슬라이드 순서 모달에 전달
-  }
-  const modalMapTag = (getData) => { 
-    let tagData = getData.map((e)=>{
-      let createListData = createList(e);
-      return createListData;
-    });
-    return tagData;
-  }
-}
-
-const dimmedFunc = (dimmedOnOff, offBtn) => { //
-  let body = document.getElementsByTagName('body')[0];
-  let dimmedTag = createTag("div","dimmed");
-  let dimmedChk = body.querySelector('.dimmed');
-  
-  console.log(dimmedChk)
-
-  if (dimmedOnOff == "open"){
-    if(dimmedChk == null ) body.appendChild(dimmedTag); // dimme 생성
-  }else{
-    //console.log("닫기")
-    //let test = document.querySelector('.modal-layer__closed button');
-    //test.removeEventListener("click",dimmedFunc)
-    // dimmedChk.remove();
-  }
-}
-/* e: modalLayer */
+// 5 swiper 
 
 
-// swiper 생성
-const swiperFunc = (thisSwiper, slideToNum) => {
-  let swiperWrap = thisSwiper.children,
-      slider1 = swiperWrap[0].childNodes,
-      initialNum = 0,
-      slidesView = 1,
-      slideeBetween = 0,
-      prevBtn = createTag("div","swiper-button-prev"),
-      nextBtn = createTag("div","swiper-button-next"),
-      pagination = createTag("div","swiper-pagination");
 
-  thisSwiper.classList.add("swiper");
-  swiperWrap[0].classList.add("swiper-wrapper");
-  slider1.forEach((el, idx) => {
-    el.classList.add("swiper-slide");
-  })
+const swiperFunc = (swiperCm, newOpt) =>{
+  const thisSwiper = swiperCm.querySelector('.swiper');
 
-  if(thisSwiper.dataset.slidesperview !== undefined){  
-    slidesView = Number(thisSwiper.dataset.slidesperview);
-    slideeBetween = Number(thisSwiper.dataset.spacebetween);
-  }
-  if(thisSwiper.dataset.navigation == "on"){
+  if(newOpt.navigation !== false){
+    let prevBtn = commonCreateTag("div","swiper-button-prev"),
+        nextBtn = commonCreateTag("div","swiper-button-next");
     thisSwiper.appendChild(prevBtn);
     thisSwiper.appendChild(nextBtn);
   }
-  if(thisSwiper.dataset.pagination == "on"){
+  if(newOpt.pagination !== false){
+    let pagination = commonCreateTag("div","swiper-pagination");
     thisSwiper.appendChild(pagination);
   }
-  if(slideToNum !== undefined) initialNum = slideToNum;
-  const swiper = new Swiper(thisSwiper,{ 
-    initialSlide : initialNum,
-    slidesPerView: slidesView,
-    spaceBetween : slideeBetween,
+  // 기본 옵션
+  let optionBase = {
+    initialSlide : 0,
+    slidesPerView : 1,
+    spaceBetween : 0,
+    loop : false,
+    loopAdditionalSlides : 1,
+    centeredSlides: false,
     navigation: {
-      nextEl: thisSwiper.querySelector('.swiper-button-next'),
-      prevEl: thisSwiper.querySelector('.swiper-button-prev'),
+        nextEl: swiperCm.querySelector('.swiper-button-next'),
+        prevEl: swiperCm.querySelector('.swiper-button-prev'),
     },
     pagination: {
-      el: thisSwiper.querySelector(".swiper-pagination"),
+      el: swiperCm.querySelector(".swiper-pagination"),
+      clickable: true,
     },
+    watchOverflow : true, // 슬라이드 수가 충분하지 않을경우 비활성화
+  }
+
+  let swiperOpt = Object.assign({}, optionBase, newOpt)  // 기본 옵션 -> 변경된 옵션 합치기
+  const newSwiper = new Swiper(thisSwiper,swiperOpt);
+};
+  
+// 객체 합치기 및 병합
+var obj1 = {
+  fruits: ['Banana', 'Mango'],
+  vegetables: "tttt",
+};
+var obj2 = {
+  vegetables: 'Walmart',
+};
+
+var obj3 = Object.assign({}, obj1, obj2);
+console.log(obj3)
+
+
+// result start
+const init = (() => {  
+  serverDataArr.forEach((dataUrl,dataIdx) => { // url에 맞게 컴포넌트 여러개 생성
+    if(dataIdx == "2") return
+    getDataLoad(dataUrl, dataIdx);
   });
-}
+})();
 
-
-// 태그 생성 및 클래스, 속성 추가  : div, class, [[attr, attrName],[attr2,attrName2]]
-const createTag = (tagName, tagClass, tagAttr, tagText) => { 
+// common : 함수명 common 시작
+// common - 간단한 class 가진 태그 생성
+function commonCreateTag(tagName, tagClass){ 
   let returnTag = document.createElement(tagName);
   if(tagClass !== undefined) returnTag.className = tagClass;
-  if(tagAttr !== undefined){
-    tagAttr.map((attr,idx) => {
-      returnTag.setAttribute(attr[0] , attr[1])
-    });
-  }
-  if(tagText !== undefined){
-    returnTag.innerHTML = tagText;
-  }
   return returnTag;
 }
 
-window.addEventListener('DOMContentLoaded', () => componentFunc());
-
- 
 
 
+
+
+
+/*
+var clonedNodesArray = jQuery.extend({}, nodesArray);
+  
+const newArray = myArray.map(a => Object.assign({}, a));
+
+$.fn.extend({
+  tubeSlider : function(name){	// 슬라이드 
+    // ※ default - 값 셋팅
+    var d = $.extend({			
+      baseTit : 'tubeSlider',	
+      object : 'sldider',			// 이용자 지정 이름 
+      motionType : 'slider',		// slider, fade 
+      viewSlide : 1,				// 보여지는 슬라이드 
+      sliderW : 'auto',			// li 사이즈 
+      arrows : true,				// 좌우 버튼 (이전, 다음) 
+      swipe : true,
+      indicator : true,			// 인디케이터 Btn  
+      infinite : true,				// 무한 반복 
+      autoPlay : false,			// 무한 반복 일 때 - 자동 롤링 
+      autoBtn : true,			// 무한 반복 일 때 - 자동 롤링 + 버튼 x 
+      time : 2000,				// 자동 롤링 시간 
+      transition : [				// 모션 동작 제어
+       { 
+        speed : 500,			// 롤링 속도 
+        easing : 'cubic-bezier(.33,.53,.47,.83)' 
+       }
+      ]
+    }, name);
+
+$test.tubeSlider({
+      object : "th", 
+      arrows: true, 
+      indicator : true,
+      infinite : true,
+      autoPlay : false
+    });
+
+
+let objArray = [ {a:1} , {b:2} ];
+
+let refArray = objArray; // this will just point to the objArray
+let clonedArray = [...objArray]; // will clone the array
+
+console.log( "before:" );
+console.log( "obj array" , objArray );
+console.log( "ref array" , refArray );
+console.log( "cloned array" , clonedArray );
+
+objArray[0] = {c:3};
+
+console.log( "after:" );
+console.log( "obj array" , objArray ); // [ {c:3} , {b:2} ]
+console.log( "ref array" , refArray ); // [ {c:3} , {b:2} ]
+console.log( "cloned array" , clonedArray ); // [ {a:1} , {b:2} ]
+
+*/
